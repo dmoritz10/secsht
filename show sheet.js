047432548fab51ab50169ba43bf7461ded223e80
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-02-21 06:56:33 -0800

    dan

--- show sheet.js
+++ show sheet.js
@@ -36,7 +36,7 @@ async function showSheet(idx) {
     .setHeader()
     .setTableHeaderClass()
     .setData(sht)
-    .setTableClass('table ')
+    .setTableClass('table table-borderless')
     .setTrClass('d-flex')
     .setTcClass(['text-end col-4 h5 text-success', 'text-start col h4', 'col-1'])
     .setTdClass('py-1 pb-0 border-0 align-bottom border-bottom')
