632f87a94cdc51b8500ef809863a8e4024b0d8d9
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-02-25 16:06:09 -0800

    dan

--- app.js
+++ app.js
@@ -244,7 +244,17 @@ jQuery(function ($) {
             $("#myToast").on("hidden.bs.toast", function() {
               $(this).addClass("d-none");
                   })
-          
+ 
+            $('.modal').on('shown.bs.modal', function () {
+                $(this).find('[autofocus]').focus();
+            });
+            $('.modal').on('show.bs.modal', function (e) {
+                var activeElement = document.activeElement;
+                $(this).on('hidden.bs.modal', function () {
+                    activeElement.focus();
+                    $(this).off('hidden.bs.modal');
+                });
+            });
           
             Date.prototype.toLocaleISOString = function() {
                 const zOffsetMs = this.getTimezoneOffset() * 60 * 1000;
