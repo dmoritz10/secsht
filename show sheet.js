9a232c37477f973ce8fb738402fa9755b9f14f14
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-02-27 08:30:30 -0800

    dan

--- show sheet.js
+++ show sheet.js
@@ -62,7 +62,7 @@ function setPrevNextBtns(dir) {
 
   var shtRows = secSht[title].rows*1 - 1
 
-  console.log('setPrevNextBtns', idx, title, shtRows, idx+1, idx-1)
+  console.log('setPrevNextBtns', idx, title, shtRows, idx+1, idx-1, idx+1 < shtRows, idx-1 > 0)
 
   var nextIdx = idx+1 < shtRows ? idx++ : null
   var prevIdx = idx-1 > 0       ? idx-- : null
