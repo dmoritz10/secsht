5b446611764540043e1924e8290fcb178ce447e9
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-02-28 09:04:45 -0800

    dan

--- show sheet.js
+++ show sheet.js
@@ -1,14 +1,13 @@
 async function showSheet(idx, title) {
 
-  console.log("shtContainer get22", $("#shtContainer").children().get(2))
+  console.log("shtContainer class", $("#shtContainer .my-2 .border"))
 
-  var x = $("#shtContainer").children()
+  var x = $("#shtContainer  .my-2 .border")
 
   console.log('x', x.get(2))
-  console.log('x', x.get(2).parent())
   
 
-  $("#shtContainer").get(2).addClass('bg-primary')
+  $("#shtContainer  .my-2 .border").get(2).addClass('bg-primary')
 
   if (idx === null) return                  // null is from browseProvider
 
