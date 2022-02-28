99c3e09debaff639b3ca7d8413d6cf6e9ec14dcd
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-02-28 07:47:42 -0800

    dan

--- show sheet.js
+++ show sheet.js
@@ -1,6 +1,6 @@
 async function showSheet(idx, title) {
 
-  console.log("shtSrchProvider", $("shtSrchProvider"))
+  console.log("shtContainer", $("shtContainer").children())
 
   if (idx === null) return                  // null is from browseProvider
 
