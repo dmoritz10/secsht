a390f61780ce18a850c067bf5bd4e886b7fe528b
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-01 09:12:48 -0800

    dan

--- show sheet.js
+++ show sheet.js
@@ -52,7 +52,7 @@ async function showSheet(idx, shtIdx) {
 
 function browseProvider(dir) {
 
-  var idx   = $("#ssIdx").val()*1
+  var idx   = $("#shtIdx").val()*1
   var title = $('#shtTitle').text()
 
   var shtRows = secSht[title].rows*1 - 1   // .rows includes hdrs
