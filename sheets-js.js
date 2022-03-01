fe9dda898e9b5f0196b24b37e3f14eecb9e95063
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-02-28 16:50:21 -0800

    dan

--- sheets-js.js
+++ sheets-js.js
@@ -225,8 +225,8 @@ async function btnShtmSubmitSheetHtml() {
 
   if (!$('#sheet-form').valid()) return
 
-  var arrIdx = $('#shtmArrIdx').val()
-  var shtIdx = $('#shtmShtIdx').val()
+  var arrIdx = $('#shtmArrIdx').val()*1
+  var shtIdx = $('#shtmShtIdx').val()*1
 
 
   if (arrIdx) {                                                       // update existing course
