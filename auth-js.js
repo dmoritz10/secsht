e7e8a9a74d5b5d5592bd09b84c24f09e98d339c5
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-01 15:34:35 -0800

    dan

--- auth-js.js
+++ auth-js.js
@@ -18,64 +18,47 @@ function btnSignoutHtml(event) {
       
 async function showLogin() {
 
-var getLogin =  await login()
+  var login = bootbox.dialog({
+    message: $(".login-form").html(),
+    title: "Login",
+    closeButton: false,
+    centerVertical: true,
+    buttons: [
+      {
+        label: "OK",
+        className: "btn btn-primary pull-left",
+        callback: function() {
+  
+          if (true) {
+  
+            return(true)
+  
+          } else {
+         
+            return (false)
+          }
+        }
+      },
+      {
+        label: "Cancel",
+        className: "btn btn-default pull-left",
+        callback: function() {
+          return(false);
+        }
+      }
+    ],
+    show: false,
+    onEscape: function() {
+      modal.login("hide");
+    }
+  });
 
+  modal.login("show");
 
 
 console.log('getLogin', getLogin)
 
 
-
-  // prompt for user Name
-  var userName = await prompt("User Name", "text");
-
-  // var userName = 'dmoritz10'
-
-  var rtn = await getSSId(userName);
-
-  if (rtn.fileId) {spreadsheetId = rtn.fileId}
-  else {$('#authSigninStatus').html(rtn.msg);return}
-
-  var ui = await initialUI();
-
-  var x = arrOptions.shtList
-  var t = "The quick brown fox jumped over the lazy dog"
-        
-  if (x == t) {
-    var pwd = await verifyCurrPwd(pwdText, "Password has not be setup.  Enter Password")
-    if (!pwd) return
-  
-    var pdrcnfrm = await prompt("Confirm Password", "password");
-    if (pwd != pdrcnfrm) {
-      var confirmOK = await confirm("Passwords don't match.")
-      return
-    }
-    var encPwd = await encryptMessage(t, pwd)
-
-    console.log('enPwd', encPwd)
-    await updateOption('shtList', encPwd)
-    x = encPwd
-  } else {
-    var pwd = await prompt("Enter Password", "password");
-  }
-// var pwd = "tempdm10"
-
-  var dx = await decryptMessage(x, pwd)
-
-  if (dx != t) {await confirm("Invalid password");return}
-
-  currUser.usr = userName
-  currUser.pwd = pwd
-
-console.log('pre loadsheets')
-
-  await loadSheets()
-
-console.log('post loadsheets')
-
-  goHome()
-
-console.log('post  goHome')
 
 }
 
