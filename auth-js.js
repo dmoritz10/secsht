78d7bd6ac1f703e9550ffc573f4e1d8eb6e0c5ae
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-01 17:07:40 -0800

    dan

--- auth-js.js
+++ auth-js.js
@@ -18,65 +18,12 @@ function btnSignoutHtml(event) {
       
 async function showLogin() {
 
-  var login = bootbox.confirm({
-    message: $(".login-form").html(),
-    title: "Login",
-    closeButton: false,
-    centerVertical: true,
-    buttons: [
-      {
-        confirm: {
-          Label: "Confirm",
-        className: "btn btn-primary pull-left"},
-        callback: async function(result) {
+  var bootboxHtml = $(".login-form").html()
 
-       
-         
-          var usr = $('#liUser', '.form').val()
-          var pwd = $('#liPassword', '.form').val()
-          
-          console.log('usr', usr);
-          console.log('pwd', pwd);
-          
-          var rtn = await getSSId(usr)
-          
-          if (rtn.fileId) {spreadsheetId = rtn.fileId}
-          else {$('#authSigninStatus').html(rtn.msg);return}
-        
-          var ui = await initialUI();
-
-          var dx = await decryptMessage(x, pwd)
-          var t = "The quick brown fox jumped over the lazy dog"
-          if (dx != t) {await confirm("Invalid password");return}
-        
-          currUser.usr = userName
-          currUser.pwd = pwd
-
-          await loadSheets()
-
-          console.log('post loadsheets')
-
-          goHome()    
-
-        }
-      },
-      {
-        cancel: {
-          label: "Cancel",
-        className: "btn btn-default pull-left"},
-        callback: function() {
-          alert('Cancel')
-          login.modal("hide");
-        }
-      }
-    ],
-    show: false,
-    onEscape: function() {
-      login.modal("hide");
-    }
+  bootbox.confirm(bootboxHtml, function(result) {
+    console.log($('#liUser', '.form').val());
+    console.log($('#liPassword', '.form').val());
   });
-
-  login.modal("show");
 
 }
 
