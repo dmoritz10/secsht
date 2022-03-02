ba1b2539bd7b4a6a08be0abfb190036918723f6b
dmoritz10 <62400066+dmoritz10@users.noreply.github.com>
2022-03-02 12:21:12 -0800

    dan

--- app.js
+++ app.js
@@ -23,6 +23,9 @@
     var timerStart
 
     var signin
+
+    var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
+
  
 
 /*global jQuery */
--- auth-js.js
+++ auth-js.js
@@ -59,8 +59,7 @@ async function submitLogin() {
     return
   } 
   
-  var strongRegex = new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})");
-  if (!strongRegex.test(pwd)) {
+  if (cfrmPwdMode && !strongRegex.test(pwd)) {
     $('#liMsg').html("Password must contain 8 or more characters")
     return
   } 
